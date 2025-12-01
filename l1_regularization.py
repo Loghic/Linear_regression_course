6aeaa62c4dcee88bff0a7ee8bb610d2731309040
Loghic <michalek.matej.loghin@gmail.com>
2025-12-01 22:33:59 +0100

    ADD l1 regularization

---
+++ l1_regularization.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Fat matrix
+N = 50
+D = 50
+
+X = (np.random.random((N,D)) - 0.5)*10
+
+true_w = np.array([1, 0.5, -0.5] + [0]*(D-3))
+
+Y = X.dot(true_w) + np.random.randn(N)*0.5
+
+# Gradient disent
+costs = []
+w = np.random.randn(D) / np.sqrt(D)
+learning_rate = 0.001
+l1 = 10.0
+for t in range(500):
+    Yhat = X.dot(w)
+    delta = Yhat - Y
+    w = w - learning_rate*(X.T.dot(delta) + l1*np.sign(w))
+
+    mse = delta.dot(delta) / N
+    costs.append(mse)
+
+plt.plot(costs)
+plt.show()
+
+print("Final w:", w)
+
+plt.plot(true_w, label="true w")
+plt.plot(w, label="w_map")
+plt.legend()
+plt.show()
